765a3e3977bb0ca0ecbc552a553bc26f83dd7ad9
JagjitNila92 <77108257+JagjitNila92@users.noreply.github.com>
2021-01-13 15:12:18 +0000

    Added Variables for quotes
    quotes variable and star sign variables

---
+++ script.js
@@ -0,0 +1,13 @@
+const signs = ["Capricon", "Aquarius", "pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius" ]
+
+const quotes = ["Love For All, Hatred For None",
+"Change the world by being yourself",
+"Never regret anything that made you smile.",
+"Die with memories, not dreams.",
+"Aspire to inspire before we expire",
+"Everything you can imagine is real.",
+"Simplicity is the ultimate sophistication.",
+"Whatever you do, do it well.",
+"What we think, we become",
+"All limitations are self-imposed.",
+"Tough times never last but tough people do."]
