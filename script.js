7c3e68df2c4cc8dd33ef25b9b07bbdb593313ba9
JagjitNila92 <77108257+JagjitNila92@users.noreply.github.com>
2021-01-14 13:28:09 +0000

    If Statements
    Finished article using if statements.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const signs = ["Capricon", "Aquarius", "pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius" ]
+const signs = ["Capricorn", "Aquarius", "pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius" ]
 
-const quotes = ["Love For All, Hatred For None",
-"Change the world by being yourself",
+const quotes = ["Love For All, Hatred For None.",
+"Change the world by being yourself.",
 "Never regret anything that made you smile.",
 "Die with memories, not dreams.",
 "Aspire to inspire before we expire",
@@ -11,3 +11,42 @@ const quotes = ["Love For All, Hatred For None",
 "What we think, we become",
 "All limitations are self-imposed.",
 "Tough times never last but tough people do."]
+
+const newQuote = quotes[Math.floor(Math.random() * quotes.length)];
+const newSign = signs[Math.floor(Math.random() * signs.length)];
+let msg;
+let  i;
+
+/*for ( i = 'capricorn' === newSign; i++){
+    console.log(`Congratulations your star sign is Capricorn and your random quote is: ${newQuote}`)
+}*/
+
+if (newSign === 'Capricorn') {
+    console.log(`Congratulations your star sign is Capricorn and your random quote is: ${newQuote}`)
+} if (newSign === 'Aquarius') {
+    console.log(`Congratulations your star sign is Aquarius and your random quote is: ${newQuote}`)
+} if (newSign === 'pisces') {
+    console.log(`Congratulations your star sign is pisces and your random quote is: ${newQuote}`)
+} if (newSign === 'Aries') {
+    console.log(`Congratulations your star sign is Aries and your random quote is: ${newQuote}`)
+} if (newSign === 'Taurus') {
+    console.log(`Congratulations your star sign is Taurus and your random quote is: ${newQuote}`)
+} if (newSign === 'Gemini') {
+    console.log(`Congratulations your star sign is Gemini and your random quote is: ${newQuote}`)
+} if (newSign === 'Cancer') {
+    console.log(`Congratulations your star sign is Cancer and your random quote is: ${newQuote}`)
+} if (newSign === 'Leo') {
+    console.log(`Congratulations your star sign is Leo and your random quote is: ${newQuote}`)
+} if (newSign === 'Virgo') {
+    console.log(`Congratulations your star sign is Virgo and your random quote is: ${newQuote}`)
+} if (newSign === 'Libra') {
+    console.log(`Congratulations your star sign is Libra and your random quote is: ${newQuote}`)
+} if (newSign === 'Scorpio') {
+    console.log(`Congratulations your star sign is Scorpio and your random quote is: ${newQuote}`)
+} if (newSign === 'Sagittarius') {
+    console.log(`Congratulations your star sign is Sagittarius and your random quote is: ${newQuote}`)
+} 
+
+//console.log(newSign);
+//console.log(newQuote);
+
